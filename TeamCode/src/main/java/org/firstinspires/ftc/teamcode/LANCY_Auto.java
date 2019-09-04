ff82cb06408584514ffbd5c858c2da74189c5fe4
Nabo Yu <54687548+naboyu@users.noreply.github.com>
2019-09-03 19:31:28 -0700

    09/03/2019

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LANCY_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LANCY_Auto.java
@@ -1,34 +1,89 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
 /*
 This is the Autonomous code for Group LANCY (Liz, Andrew, Nathan, Cristian, and Yoyo)
  */
 @Autonomous(name = "Autonomous")
-public class LANCY_Auto extends OpMode {
+public class LANCY_Auto extends LinearOpMode {
 
+    //Declare motors
     DcMotor motorLeft = null;
     DcMotor motorRight = null;
-    DcMotor motorC = null;
+    //Set drive power
+    double drivePower = 0.5;
+    //Vuforia variables
+    private VuforiaLocalizer vuforiaLocalizer;
+    private VuforiaLocalizer.Parameters parameters;
+    private VuforiaTrackables visionTargets;
+    private VuforiaTrackable target;
+    private VuforiaTrackableDefaultListener listener;
 
-    @Override public void init() {
+    private OpenGLMatrix lastKnownLocation;
+    private OpenGLMatrix phoneLocation;
 
+    private static final String vuforiaKey = "";
 
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+        while (opModeIsActive()) {
+            telemetry.update();
+        }
     }
 
-    public void DriveForward() {
-
+    //Drive Forwards
+    public void DriveForward(double power) {
+        motorLeft.setPower(power);
+        motorRight.setPower(power);
     }
 
-    public void loop() {
+    //Drive Backwards
+    public void DriveReverse(double power) {
+        motorLeft.setPower(-power);
+        motorRight.setPower(-power);
+    }
 
+    //Turn Left
+    public void TurnLeft(double power) {
+        motorLeft.setPower(-power);
+        motorRight.setPower(power);
+    }
 
+    //Turn Right
+    public void TurnRight(double power) {
+        motorLeft.setPower(power);
+        motorRight.setPower(-power);
+    }
+
+    //StopDriving
+    public void Brake() {
+        DriveForward(0);
     }
 
 }
 
+/* Unused Code
 
-// I like Mi4i
+    DcMotor motorC = null;
+
+    //Intake Arm Lift
+    public void ArmLift(double power) {
+        motorC.setPower(power);
+    }
+
+    //Intake Arm Down
+    public void ArmDown(double power) {
+        motorC.setPower(-power);
+    }
+
+ */
