3fd42ca5d2070a23666ba9aa8842c23fe9acf3b4
nathanliu <neinei2017>
2019-09-05 15:20:03 -0700

    09/05/2019 Almost There.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LANCY_TeleOp_NL.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LANCY_TeleOp_NL.java
@@ -1,15 +1,16 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /*
 This is the TeleOp code for Group LANCY (Liz, Andrew, Nathan, Cristian, and Yoyo)
  */
 
 @TeleOp(name = "TeleOp", group = "Lancy")
+@Disabled
 public class LANCY_TeleOp_NL extends LinearOpMode
 {
     private DcMotor motorLeft;
