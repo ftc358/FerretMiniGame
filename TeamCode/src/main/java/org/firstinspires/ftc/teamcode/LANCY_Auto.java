9377285f3b3fbd8aacc0dd2585f7a515b16079cf
Nabo Yu <54687548+naboyu@users.noreply.github.com>
2019-09-04 17:44:05 -0700

    09/04/2019 Vuforia Secondary

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LANCY_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LANCY_Auto.java
@@ -3,8 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.vuforia.HINT;
-import com.vuforia.Vuforia;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
@@ -24,7 +22,11 @@ This is the Autonomous code for Group LANCY (Liz, Andrew, Nathan, Cristian, and 
 public class LANCY_Auto extends LinearOpMode {
 
     //Vuforia Key
-    private static final String vuforiaKey = "";
+    private static final String vuforiaKey =
+            "AXzW9CD/////AAAAGTPAtr9HRUXZmowtd9p0AUwuXiBVONS/c5x1q8OvjMrQ8"
+                    + "/XJGxEp0TP9Kl8PvqSzeXOWIvVa3AeB6MyAQboyW/Pgd/c4a4U"
+                    +
+                    "/VBs1ouUsVBkEdbaq1iY7RR0cjYr3eLwEt6tmI37Ugbwrd5gmxYvOBQkGqzpbg2U2bVLycc5PkOixu7PqPqaINGZYSlvUzEMAenLOCxZFpsayuCPRbWz6Z9UJfLeAbfAPmmDYoKNXRFll8/jp5Ie7iAhSQgfFggWwyiqMRCFA3GPTsOJS4H1tSiGlMjVzbJnkusPKXfJ0dK3OH9u7ox9ESpi91T0MemXw3nn+/6QRvjGtgFH+wMDuQX7ta89+yW+wqdXX9ZQu8BzY";
     //Declare motors
     DcMotor motorLeft = null;
     DcMotor motorRight = null;
@@ -41,19 +43,29 @@ public class LANCY_Auto extends LinearOpMode {
 
     public void runOpMode() throws InterruptedException {
         setupVuforia();
+        runUsingEncoders();
         //setting robot location to origin
         lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
         waitForStart();
+        OpenGLMatrix originalLocation = listener.getUpdatedRobotLocation();
+        //Monitor position using Vuforia (most likely obsolete)
         while (opModeIsActive()) {
             OpenGLMatrix latestLocation = listener.getUpdatedRobotLocation();
             // For the listener to not return null, checking for it to prevent errors
-            if(latestLocation != null)
+            if (latestLocation != null) {
                 lastKnownLocation = latestLocation;
+            }
             // Tell if the target is visible, and where the robot is
             telemetry.addData("Tracking " + target.getName(), listener.isVisible());
             telemetry.addData("Last Known Location", formatMatrix(lastKnownLocation));
             telemetry.update();
             idle();
+        }
+        if (originalLocation.get(0, 0) > originalLocation.get(0, 0)) {
+
+        }
+        else {
+
         }
     }
 
@@ -69,7 +81,6 @@ public class LANCY_Auto extends LinearOpMode {
         // These are the vision targets that we want to use
         // The string needs to be the name of the appropriate .xml file in the assets folder
         visionTargets = vuforiaLocalizer.loadTrackablesFromAsset(""); //We need object file
-        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
 
         // Setup the target to be tracked
         target = visionTargets.get(0); // 0 corresponds to the wheels target
@@ -77,7 +88,7 @@ public class LANCY_Auto extends LinearOpMode {
         target.setLocation(createMatrix(0, 0, 0, 0, 0, 0));
 
         // Set phone location on robot
-        phoneLocation = createMatrix(0, 0, 0, 0, 0, 0);
+        phoneLocation = createMatrix(-127, 51, 0, 90, 0, 0);
 
         // Setup listener and inform it of phone information
         listener = (VuforiaTrackableDefaultListener) target.getListener();
@@ -90,8 +101,7 @@ public class LANCY_Auto extends LinearOpMode {
                         AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
     }
 
-    private String formatMatrix(OpenGLMatrix matrix)
-    {
+    private String formatMatrix(OpenGLMatrix matrix) {
         return matrix.formatAsTransform();
     }
 
@@ -126,6 +136,18 @@ public class LANCY_Auto extends LinearOpMode {
     //StopDriving
     public void Brake() {
         DriveForward(0);
+    }
+
+    // Run Using Encoders
+    public void runUsingEncoders() {
+        motorLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    // Run Without Encoders
+    public void runWithoutEncoders() {
+        motorLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motorRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
     }
 
 }
